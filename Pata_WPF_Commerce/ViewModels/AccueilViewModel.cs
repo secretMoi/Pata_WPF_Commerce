eda2158ad7e91f5a1c482b125f9e7601bc111875
Quentin <win.moi@hotmail.com>
2020-10-22 15:54:06 +0200

    Ajout d'une menu Ajout d'une vérification des stocks

--- Pata_WPF_Commerce/ViewModels/AccueilViewModel.cs
+++ Pata_WPF_Commerce/ViewModels/AccueilViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Pata_WPF_Commerce.Views;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Pata_WPF_Commerce.Repositories;
+using Pata_WPF_Commerce.Views;
 
 namespace Pata_WPF_Commerce.ViewModels
 {
@@ -11,6 +15,22 @@ namespace Pata_WPF_Commerce.ViewModels
 			CommandStocks = new BaseCommand(Stocks);
 			CommandAchats = new BaseCommand(Achats);
 			CommandVentes = new BaseCommand(Ventes);
+			CommandCheckStock = new BaseCommand(CheckStock);
+		}
+
+		public void CheckStock()
+		{
+			IList<Database.Classes.Stock> stocks = StocksRepository.Instance.Lire().Where(item => item.QuantiteActuelle < item.QuantiteMin).ToList();
+
+			if (stocks.Count == 0) return;
+
+			var result = MessageBox.Show(
+				"Il existe des stocks en quantité insuffisantes !\nVoulez-vous aller sur la fenêtre de stock ?",
+				"Attention",
+				MessageBoxButton.YesNo);
+
+			if(result == MessageBoxResult.Yes)
+				Stocks();
 		}
 
 		public void Clients()
@@ -41,5 +61,6 @@ namespace Pata_WPF_Commerce.ViewModels
 		public BaseCommand CommandStocks { get; set; }
 		public BaseCommand CommandAchats { get; set; }
 		public BaseCommand CommandVentes { get; set; }
+		public BaseCommand CommandCheckStock { get; set; }
 	}
 }
