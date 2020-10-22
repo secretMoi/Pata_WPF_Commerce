9bde5f0ffca27ede3d931e30a1155f1864b948b5
Quentin <win.moi@hotmail.com>
2020-10-22 16:16:48 +0200

    Ajout des classes pdf et html

---
+++ Pata_WPF_Commerce/Core/HtmlView.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using System.Text;
+using iText.Kernel.Colors;
+using Color = System.Windows.Media.Color;
+
+namespace Pata_WPF_Commerce.Core
+{
+	public class HtmlView
+	{
+		private StringBuilder _htmlCode;
+		private int _nombreColonnes;
+		private int _colonneActuelle;
+
+		public HtmlView(string titre, int nombreColonnes)
+		{
+			_nombreColonnes = nombreColonnes;
+			GenerateHead(titre);
+		}
+
+		private void GenerateHead(string titre)
+		{
+			_htmlCode = new StringBuilder();
+
+			_htmlCode.Append(
+				"<!doctype html>\r\n" +
+				"<html lang=\"fr\">\r\n" +
+				"<head>\r\n" +
+				"<meta charset=\"utf-8\">\r\n" +
+				"<title>" + titre + "</title>\r\n" +
+				"</head>\r\n" +
+				"<body>\r\n" +
+				"<h1 align='center'>" + titre + "</h1>\r\n" +
+				"<table align='center' cellpadding='5' cellspacing='0' style='border: 1px solid " +
+				ColorToHex(Color.FromRgb(0, 0, 0)) + ";'>\r\n"
+			);
+		}
+
+		// converti la classe couleur en code couleur hexa
+		public static string ColorToHex(Color color)
+		{
+			return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+		}
+
+		public void GenerateColumn(params string[] data)
+		{
+			if (data.Length != _nombreColonnes) return;
+
+			_htmlCode.Append("<tr>\r\n");
+
+			foreach (string colonne in data)
+			{
+				_htmlCode.Append(
+					"<th style='color: " + ColorToHex(Color.FromRgb(0, 0, 0)) + " ;background-color: " + ColorToHex(Color.FromRgb(255,255,255)) + "; border: 1px solid " + ColorToHex(Color.FromRgb(0, 0, 0)) + ";'>" +
+					colonne +
+					"</th>\r\n"
+				);
+			}
+
+			_htmlCode.Append("</tr>\r\n\r\n");
+		}
+
+		public void GenerateBody(string data)
+		{
+			if (_colonneActuelle % _nombreColonnes == 0)
+			{
+				_htmlCode.Append("<tr>\r\n");
+
+				if (_colonneActuelle != 0)
+					_htmlCode.Append("</tr>\r\n\r\n");
+			}
+
+			_htmlCode.Append(
+				"<td style='border: 1px solid " + ColorToHex(Color.FromRgb(0, 0, 0)) + "'>" +
+				data +
+				"</td>\r\n"
+			);
+
+			_colonneActuelle++;
+		}
+
+		public void GenerateFooter()
+		{
+			_htmlCode.Append(
+				"</tr>\r\n\r\n" +
+				"</table>\r\n" +
+				"</body>\r\n" +
+				"</html>\r\n"
+			);
+		}
+
+		public void SaveTo(string file)
+		{
+			GenerateFooter();
+			File.WriteAllText(file + ".html", _htmlCode.ToString());
+		}
+
+		public string SourceCode => _htmlCode.ToString();
+
+		public int NombreColonnes
+		{
+			get => _nombreColonnes;
+			set => _nombreColonnes = value;
+		}
+	}
+}
---
+++ Pata_WPF_Commerce/Core/Pdf.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using iText.IO.Image;
+using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Colorspace;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace Pata_WPF_Commerce.Core
+{
+	public class Pdf
+	{
+		private readonly Document _document;
+
+		private Table _table;
+		private List<Cell> _cells;
+
+		private readonly List<object> _elements = new List<object>();
+
+		public Pdf(string path)
+		{
+			PdfWriter writer = new PdfWriter( // création du fichier pdf
+				path + ".pdf"
+			);
+
+
+			PdfDocument pdf = new PdfDocument(writer); // permet de manipuler un document pdf
+			_document = new Document(pdf); // permet de s'abstraire aux limites pdf
+		}
+
+		public bool Logo(string name)
+		{
+			string path = @"Ressources/Images/" + name + ".png";
+
+			if (!File.Exists(path)) return false;
+
+			Image logo = new Image(ImageDataFactory.Create(path))
+				.SetHeight(100)
+				.SetWidth(100)
+				.SetTextAlignment(TextAlignment.LEFT);
+
+			_elements.Add(logo);
+
+			return true;
+		}
+
+		public void RightColumn(StringBuilder text)
+		{
+			RightColumn(text.ToString());
+		}
+
+		public void RightColumn(string text)
+		{
+			Paragraph paragraph = new Paragraph(text)
+				.SetMarginLeft(400)
+				.SetMarginBottom(10)
+				.SetTextAlignment(TextAlignment.LEFT)
+				.SetFontSize(12);
+
+			_elements.Add(paragraph);
+		}
+
+		public void Title(string title)
+		{
+			Paragraph paragraph = new Paragraph(title)
+				.SetMarginBottom(10)
+				.SetTextAlignment(TextAlignment.CENTER)
+				.SetFontSize(16);
+
+			_elements.Add(paragraph);
+		}
+
+		public bool MakeTable(int columnNumber)
+		{
+			if (columnNumber < 1) return false;
+
+			_table = new Table(columnNumber, false).UseAllAvailableWidth();
+
+			_elements.Add(_table);
+
+			return true;
+		}
+
+		public bool MakeTableHeader(string header)
+		{
+			if (_table == null) return false;
+
+			if (_cells == null) _cells = new List<Cell>();
+
+			Cell cell = new Cell(1, 1)
+				/*.SetBackgroundColor(Color.MakeColor(new PdfDeviceCs.Gray())) // couleur arrière-plan
+				.SetFontColor(new DeviceRgb(System.Windows.Media.Color.FromRgb(0, 0, 0))) // couleur texte*/
+				.Add(new Paragraph(header)); // texte
+
+			_table.AddCell(cell);
+
+			return true;
+		}
+
+		public bool MakeTableData(string data)
+		{
+			if (_table == null) return false;
+
+			Cell cell = new Cell(1, 1)
+				//.SetFontColor(new DeviceRgb(Theme.BackDark)) // couleur texte
+				.Add(new Paragraph(data)); // texte
+
+			_table.AddCell(cell);
+
+			return true;
+		}
+
+		public void Close()
+		{
+			foreach (object element in _elements)
+				_document.Add((dynamic)element); // ligne magique pour convertir l'objet en son premier type
+
+			_document.Close();
+		}
+
+		public static void DirectoryAvailable(string directory)
+		{
+			if (!Directory.Exists(directory))
+				Directory.CreateDirectory(directory);
+		}
+
+		public static string SetDate()
+		{
+			string date = DateTime.Now.ToString(CultureInfo.InvariantCulture).Replace(':', '_');
+			return date.Replace('/', '_');
+		}
+	}
+}
