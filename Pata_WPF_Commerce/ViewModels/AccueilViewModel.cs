08c986a5493b336245755175a7f5014c7f5acee1
Quentin <win.moi@hotmail.com>
2020-10-23 10:36:06 +0200

    Ajout du chiffre d'affaire

--- Pata_WPF_Commerce/ViewModels/AccueilViewModel.cs
+++ Pata_WPF_Commerce/ViewModels/AccueilViewModel.cs
@@ -1,15 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
+using Pata_WPF_Commerce.Core;
 using Pata_WPF_Commerce.Repositories;
+using Pata_WPF_Commerce.ViewModels.DataBinding;
 using Pata_WPF_Commerce.Views;
 
 namespace Pata_WPF_Commerce.ViewModels
 {
 	public class AccueilViewModel : BaseProperty
 	{
+		private DataAccueil _item = new DataAccueil();
+		private readonly CommandesVentesRepository _commandesVentesRepository = CommandesVentesRepository.Instance;
+		private readonly DetailVentesRepository _ventesRepository = DetailVentesRepository.Instance;
+
+		public DataAccueil Item // élément sélectionné dans la dgv
+		{
+			get => _item;
+			set => AssignField(ref _item, value, MethodBase.GetCurrentMethod().Name);
+		}
+
 		public AccueilViewModel()
 		{
+			Item.ChiffreAffaire = Money.Display(ChiffreAffaireHebdomadaire());
+
 			// bind les commandes au xaml
 			CommandClients = new BaseCommand(Clients);
 			CommandStocks = new BaseCommand(Stocks);
@@ -18,11 +34,43 @@ namespace Pata_WPF_Commerce.ViewModels
 			CommandCheckStock = new BaseCommand(CheckStock);
 		}
 
+		/**
+		 * <summary>Calcul le chiffre d'affaire de la semaine</summary>
+		 */
+		private decimal ChiffreAffaireHebdomadaire()
+		{
+			// récupère la liste des ventes des 7 derniers jours
+			var commandesVentes =
+				_commandesVentesRepository.Lire().Where(item => item.Date >= DateTime.Now.AddDays(-7)).ToList();
+
+			decimal chiffreAffaire = 0;
+			foreach (var commande in commandesVentes)
+			{
+				// récupère les articles vendus par chaque commande
+				var ventes = _ventesRepository.Lire().Where(item => item.IdCommande == commande.Id).ToList();
+
+				// pour chaque élément vendu
+				foreach (var vente in ventes)
+					chiffreAffaire += vente.Prix * vente.Quantite;
+			}
+
+			return chiffreAffaire;
+		}
+
+		/**
+		 * Vérifie que les stocks soient en bonne quantité
+		 */
 		public void CheckStock()
 		{
+			// récupère les stocks en quantité insuffisante
 			IList<Database.Classes.Stock> stocks = StocksRepository.Instance.Lire().Where(item => item.QuantiteActuelle < item.QuantiteMin).ToList();
 
-			if (stocks.Count == 0) return;
+			// si tous les stocks sont bons
+			if (stocks.Count == 0)
+			{
+				MessageBox.Show("Tous vos stocks sont en quantités abondantes !");
+				return;
+			}
 
 			var result = MessageBox.Show(
 				"Il existe des stocks en quantité insuffisantes !\nVoulez-vous aller sur la fenêtre de stock ?",
---
+++ Pata_WPF_Commerce/ViewModels/DataBinding/DataAccueil.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Pata_WPF_Commerce.ViewModels.DataBinding
+{
+	public class DataAccueil : BaseProperty
+	{
+		private string _chiffreAffaire;
+
+		public string ChiffreAffaire
+		{
+			get => _chiffreAffaire;
+			set => AssignField(ref _chiffreAffaire, value, MethodBase.GetCurrentMethod().Name);
+		}
+	}
+}
