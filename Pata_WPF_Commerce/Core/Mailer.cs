c39ad8b0a5a33a7d01538f0e188b6539e27a0e81
Quentin <win.moi@hotmail.com>
2020-10-25 14:19:16 +0100

    Ajout tache mail

---
+++ Pata_WPF_Commerce/Core/Mailer.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Pata_WPF_Commerce.Core
+{
+	public class Mailer
+	{
+		public string Source { get; set; }
+		public string Destination { get; set; }
+		public string Password { get; set; }
+		public string Title { get; set; }
+		public string Body { get; set; }
+
+		public async Task SendAsync()
+		{
+			if(!IsValidAdress(Destination) || !IsValidAdress(Source))
+				return;
+
+			MailMessage mail = new MailMessage
+			{
+				From = new MailAddress(Source),
+				To = { Destination },
+				Subject = Title,
+				Body = Body
+			};
+
+			SmtpClient smtp = new SmtpClient("smtp.live.com")
+			{
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				Port = 587,
+				Credentials = new NetworkCredential(Source, Password),
+				EnableSsl = true
+			};
+
+			await smtp.SendMailAsync(mail);
+		}
+
+		public static bool IsValidAdress(string mail)
+		{
+			try
+			{
+				MailAddress addr = new MailAddress(mail);
+				return addr.Address == mail;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}
